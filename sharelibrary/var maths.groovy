6316fa7dbd8df4b19a1e953f5090eaa1023aeade
JONESMOLEKA <112180574+JONESMOLEKA@users.noreply.github.com>
2022-09-04 20:30:16 -0400

    Create var maths.groovy

---
+++ sharelibrary/var maths.groovy
@@ -0,0 +1,16 @@
+def sum(a,b)
+{
+echo "the sum of $(a) and $(b) is $(a+b)"
+}
+def subtract (a,b)
+{
+echo "the subrtaction of $(a) and $(b) is $(a-b)"
+}
+deg mul (a,b)
+{
+echo "the product of $(a) and $(b) is $(a*b)"
+}
+def div(a,b)
+{
+the division of $(a) and $(b) is $(a/b)"
+}
